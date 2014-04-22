415c61b201eee2dad8753678f891d3be2b3a9ad3
Amanjeev Sethi <aj@amanjeev.com>
2014-04-21 20:58:35 -0400

    finall, a file

--- get_snps.py
+++ get_snps.py
@@ -11,6 +11,7 @@ def get_file_content(filename):
 def get_line_fields(content):
   fields = []
   for line in content:
+    line = line.rstrip()
     fields_this_line = re.split(r'\t+', line.rstrip('\t\n'))
     fields.append(fields_this_line)
   return fields
@@ -25,13 +26,23 @@ def write_file(filename, data):
   f.write(data)
   f.close()
 
-def main_snp(param, filename):
+def main_snp(param, filename, outfile):
   if param == 'snp':
     get_snp(filename)
   elif param == 'acc':
     get_acc(filename)
+  elif param == 'chrposacc':
+    get_chr_pos_acc(filename, outfile)
   else:
     get_both(filename)
+
+def get_chr_pos_acc(filename, outfile):
+  fields = get_all_lines_fields(filename)
+  data = 'Accession\tLocation\tChromosome\n'
+  for line in fields:
+    if line[-1] != 'Accession':
+      data += line[-1] + '\t' + line[-2] + '\t' + line[-3] + '\n'
+  write_file(outfile, data)
 
 def get_snp(filename):
   fields = get_all_lines_fields(filename)
@@ -51,19 +62,20 @@ def get_both(filename):
     if line[-1] != 'dbSNP' and line[-2] != 'Accession':
       data += line[-2] + '\t' + line[-1] + '\n'
   write_file('/home/scidb/proj/figs/figs/data_heart/clinvar_dbsnp_id_cv_accession_heart_disease.txt', data)
-  
+
 def usage():
   print "\nGive what you want:"
-  print "snp: snp ids; acc: clinvar accession; snpacc: both;"
+  print "snp: snp ids; acc: clinvar accession; snpacc: both; and filename at the end to process."
 
 def main(argv):
-  if len(argv) != 2:
+  if len(argv) != 4:
     usage()
     sys.exit(2)
-  elif len(argv) == 2:
-    script, param = argv
-    filename = '/home/scidb/proj/figs/figs/data_heart/clinvar_result_tabbed_file_dbsnp_heart_disease.txt'
-    main_snp(param, filename)
+  elif len(argv) == 4:
+    script, param, filename, outfile = argv
+    filename = filename
+    outfile = outfile
+    main_snp(param, filename, outfile)
   else:
     print "something is wrong and we do not know what is wrong"
     sys.exit(2)
